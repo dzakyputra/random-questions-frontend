9c89717fdd49d0e381c75d83da73d0a9c90cb937
dzaky <dzakywidyaputra@gmail.com>
2020-11-30 20:51:14 +0700

    [UPDATE] getQuestion API to the Google Cloud Run container

--- src/Components/Generator.js
+++ src/Components/Generator.js
@@ -2,21 +2,26 @@ import React from 'react';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://206.189.80.196:8080`
+    baseURL: `https://random-questions-backend-t6lmiokecq-as.a.run.app`
 })
 
 class Generator extends React.Component {
 
     state = {
         id: 0,
-        question: 'Your question will appear here'
+        question: 'Your question will appear here',
+        loading: false
     }
 
     getQuestion = async () => {
 
+        this.setState({ loading: true })
+
         let res = await api.get('/questions/random')
+        
         this.setState({ id: res.data.data.ID, 
-                        question: res.data.data.Question })
+                        question: res.data.data.Question,
+                        loading: false })
     }
 
     render () {
@@ -39,10 +44,18 @@ class Generator extends React.Component {
                                     Question ID: {this.state.id}
                                 </div> */}
                                 
-                                <p className={"block mt-1 lg:text-3xl leading-tight md:text-xl text-base text-center " + (this.state.id === 0 ? 'text-gray-400' : 'text-black')}> 
-                                    {this.state.question} 
-                                </p>
-                                
+                                <div className={"block mt-1 lg:text-3xl leading-tight md:text-xl text-base text-center " + (this.state.id === 0 ? 'text-gray-400' : 'text-black')}> 
+
+                                    { this.state.loading ? 
+
+                                      <div className="flex justify-center">
+                                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="text-blue-500 w-10 h-10 animate-spin">
+                                              <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
+                                          </svg>
+                                      </div> : this.state.question }
+                                    
+                                </div>
+
                                     {/* <p className="mt-2 text-gray-500 text-xs">#random</p> */}
                             </div>
       
